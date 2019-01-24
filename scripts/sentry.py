18176362cdfcf91e9d507e6cea8ca8e4030910d5
Banghart <banghajs@l200205.cs.jmu.edu>
2019-01-24 13:22:32 -0500

    finished lab

--- scripts/sentry.py
+++ scripts/sentry.py
@@ -9,8 +9,10 @@ Version:
 
 import rospy
 
+from kobuki_msgs.msg import Sound
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+
 import numpy as np
 
 
@@ -32,16 +34,31 @@ class SentryNode(object):
         self.cv_bridge = CvBridge()
         rospy.Subscriber('/camera/depth_registered/image',
                          Image, self.depth_callback, queue_size=1)
+        self.sound_pub = rospy.Publisher('/mobile_base/commands/sound', Sound, queue_size=1)
+        self.sound = Sound()
+        self.sound.value = 1
+        self.previous_slice = None
+        self.threshold = 2
+        self.average = 0
+        self.alpha = .75
         rospy.spin()
 
     def depth_callback(self, depth_msg):
         """ Handle depth callbacks. """
-
         # Convert the depth message to a numpy array
+        
         depth = self.cv_bridge.imgmsg_to_cv2(depth_msg)
+        current_slice = depth[:,depth_msg.width/2]
 
-        # YOUR CODE HERE.
-        # HELPER METHODS ARE GOOD.
+        if self.previous_slice != None :
+            d = current_slice - self.previous_slice
+            d = d[~np.isnan(d)]
+            d = np.linalg.norm(d)
+            self.average = self.average * self.alpha + d * (1 - self.alpha)
+            print d/self.average
+            if d/self.average > self.threshold :
+                self.sound_pub.publish(self.sound)
+        self.previous_slice = current_slice
 
 
 if __name__ == "__main__":
